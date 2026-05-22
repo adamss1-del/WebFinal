28f15d6c3d5cd4b67e7fc84085efd9e5eb6f966b
adamss1-del <adamss1@aucegypt.edu>
2026-05-22 08:22:01 -0500

    Update server.js

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -1,1 +1,24 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let registrations = [];
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.post("/register", (req, res) => {
+  registrations.push(req.body);
+  res.json({ message: "Registration successful!" });
+});
+
+app.get("/students", (req, res) => {
+  res.json(registrations);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on " + PORT));
