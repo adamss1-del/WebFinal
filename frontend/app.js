dbc87ab2952ae0baa4f9345d5e934b602cb140b7
adamss1-del <adamss1@aucegypt.edu>
2026-05-22 08:41:21 -0500

    Create app.js

---
+++ frontend/app.js
@@ -0,0 +1,24 @@
+document.getElementById("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const mode = document.querySelector('input[name="mode"]:checked').value;
+
+  const data = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    course: document.getElementById("course").value,
+    mode: mode,
+    agree: document.getElementById("agree").checked
+  };
+
+  const res = await fetch("https://webfinal-emq5.onrender.com/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+  const result = await res.json();
+  alert(result.message);
+});
