75d3e92f56aa7edd98ba36126ebd2b2bd644da07
adamss1-del <adamss1@aucegypt.edu>
2026-05-22 08:15:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,3 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log("Server running on port " + PORT));
 
-app.get("/", (req, res) => {
-  res.send("Student Registration API is running");
-});
