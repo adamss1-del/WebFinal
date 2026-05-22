e4038e13eca4227c29a404b928db1e5ee45a374c
adamss1-del <adamss1@aucegypt.edu>
2026-05-22 07:53:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -21,4 +21,6 @@ app.get("/students", (req, res) => {
   res.json(registrations);
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
